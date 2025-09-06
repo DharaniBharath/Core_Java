1477749b6ea134f1d164a5da89f7bc285c5bf0d8
DharaniBharath <bharathddb421@gmail.com>
2025-09-06 15:29:42 +0530

    added city importance

--- src/Firstjavacode.java
+++ src/Firstjavacode.java
@@ -2,9 +2,9 @@ public class Firstjavacode {
     public static void main(String[] args) {
         System.out.println("This code one");
 
-        System.out.println("vijayawada city");
+        System.out.println("vijayawada");
 
-        System.out.println("india");
+
 
 
 
